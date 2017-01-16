a266da653ac78b2fca21175246fa901776188b3a
Bronisław Białek <bronislaw.bialek@gowork.pl>
2017-01-16 16:43:52 +0100

    Add doc blocks

--- src/ArrayValue.php
+++ src/ArrayValue.php
@@ -4,47 +4,98 @@ namespace GW\Value;
 
 interface ArrayValue extends \IteratorAggregate, \Countable, \ArrayAccess
 {
-    public function map(callable $transformer): ArrayValue;
+    public function map(
+        callable/*(mixed $value): mixed*/
+        $transformer
+    ): ArrayValue;
 
-    public function filter(callable $transformer): ArrayValue;
+    public function filter(
+        callable/*(mixed $value): bool*/
+        $transformer
+    ): ArrayValue;
 
     public function slice(int $offset, int $length): ArrayValue;
 
-    public function sort(callable $comparator): ArrayValue;
+    public function sort(
+        callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator
+    ): ArrayValue;
 
-    public function each(callable $f): ArrayValue;
+    public function each(
+        callable/*(mixed $value): void*/
+        $f
+    ): ArrayValue;
 
-    public function unique(?callable $comparator = null): ArrayValue;
+    public function unique(
+        ?callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator = null
+    ): ArrayValue;
 
     public function reverse(): ArrayValue;
 
-    public function intersect(ArrayValue $other, ?callable $comparator = null): ArrayValue;
+    public function intersect(
+        ArrayValue $other,
+        ?callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator = null
+    ): ArrayValue;
 
-    public function diff(ArrayValue $other, ?callable $comparator = null): ArrayValue;
+    public function diff(
+        ArrayValue $other,
+        ?callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator = null
+    ): ArrayValue;
 
-    public function sum(ArrayValue $other): ArrayValue;
+    public function join(ArrayValue $other): ArrayValue;
 
     public function shuffle(): ArrayValue;
 
     // adders and removers
+    /**
+     * @param mixed $v
+     */
     public function unshift($v): ArrayValue;
 
+    /**
+     * @param mixed $v
+     */
     public function shift(&$v = null): ArrayValue;
 
+    /**
+     * @param mixed $v
+     */
     public function push($v): ArrayValue;
 
+    /**
+     * @param mixed $v
+     */
     public function pop(&$v = null): ArrayValue;
 
     // finalizers
-    public function reduce(callable $transformer, $start);
 
+    /**
+     * @param mixed $start
+     * @return mixed
+     */
+    public function reduce(
+        callable/*(mixed $value, mixed $reduced): mixed*/
+        $transformer,
+        $start
+    );
+
+    /**
+     * @return mixed[]
+     */
     public function toArray(): array;
 
+    public function sum(): int;
+
+    /**
+     * @return mixed
+     */
     public function first();
 
+    /**
+     * @return mixed
+     */
     public function last();
-
-    public function shiftValue();
-
-    public function popValue();
 }
--- src/AssocArray.php
+++ src/AssocArray.php
@@ -4,17 +4,37 @@ namespace GW\Value;
 
 interface AssocArray extends \IteratorAggregate, \Countable, \ArrayAccess
 {
-    public function map(callable $transformer): ArrayValue;
+    public function map(
+        callable/*(mixed $value): mixed*/
+        $transformer,
+        callable/*(string $key): string*/
+        $keyTransformer = null
+    ): ArrayValue;
 
-    public function filter(callable/*(mixed $value, string $key): bool*/ $transformer): ArrayValue;
+    public function filter(
+        callable/*(mixed $value, string $key): bool*/
+        $transformer
+    ): ArrayValue;
 
-    public function sort(callable/*(mixed $valueA, mixed $valueB): int*/ $comparator): ArrayValue;
+    public function sort(
+        callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator
+    ): ArrayValue;
 
-    public function sortKeys(callable/*(string $keyA, string $keyB): int*/ $comparator): ArrayValue;
+    public function sortKeys(
+        callable/*(string $keyA, string $keyB): int*/
+        $comparator
+    ): ArrayValue;
 
-    public function each(callable/*(mixed $value, string $key): void*/ $f): ArrayValue;
+    public function each(
+        callable/*(mixed $value, string $key): void*/
+        $f
+    ): ArrayValue;
 
-    public function unique(?callable/*(mixed $valueA, mixed $valueB): int*/ $comparator = null): ArrayValue;
+    public function unique(
+        ?callable/*(mixed $valueA, mixed $valueB): int*/
+        $comparator = null
+    ): ArrayValue;
 
     public function intersect(ArrayValue $other, ?callable $comparator = null): ArrayValue;
 
@@ -23,22 +43,45 @@ interface AssocArray extends \IteratorAggregate, \Countable, \ArrayAccess
     public function sum(ArrayValue $other): ArrayValue;
 
     // adders and removers
+    /**
+     * @param mixed $v
+     */
     public function with(string $key, $v): ArrayValue;
 
     public function without(string $key): ArrayValue;
 
+    /**
+     * @param mixed $v
+     */
     public function withoutElement($v): ArrayValue;
 
     // finalizers
-    public function reduce(callable/*(mixed $value, mixed $reduced, string $key): mixed*/ $transformer, $start);
+    /**
+     * @param mixed $start
+     * @return mixed
+     */
+    public function reduce(
+        callable/*(mixed $value, mixed $reduced, string $key): mixed*/
+        $transformer,
+        $start
+    );
 
+    /**
+     * @return mixed[]
+     */
     public function toArray(): array;
 
+    /**
+     * @return string[]|ArrayValue
+     */
     public function keys(): ArrayValue;
 
     public function values(): ArrayValue;
 
+    /**
+     * @return mixed
+     */
     public function get(string $key);
 
-    public function has(string $key);
+    public function has(string $key): bool;
 }
