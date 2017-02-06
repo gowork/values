a402b35c394ecf258ded051090173a67cda9d632
Damian Zaprzalek <damian.zaprzalek@gowork.pl>
2017-02-06 21:48:47 +0100

    AssocArray spec

--- spec/AssocArraySpec.php
+++ spec/AssocArraySpec.php
@@ -2,8 +2,8 @@
 
 namespace spec\GW\Value;
 
+use GW\Value\Arrays;
 use GW\Value\AssocArray;
-use GW\Value\PlainArray;
 use PhpSpec\ObjectBehavior;
 
 final class AssocArraySpec extends ObjectBehavior
@@ -17,13 +17,17 @@ final class AssocArraySpec extends ObjectBehavior
         ]);
     }
 
+    function it_is_initializable()
+    {
+        $this->beConstructedWith([]);
+        $this->shouldHaveType(AssocArray::class);
+    }
+
     function it_should_return_keys()
     {
         $keys = $this->keys();
         $keys->shouldNotBe($this);
-        $keys->shouldBeLike(new PlainArray([
-            'a', 'b', 'c'
-        ]));
+        $keys->shouldBeLike(Arrays::strings(['a', 'b', 'c']));
     }
 
     function it_should_return_filtered_array()
@@ -36,10 +40,12 @@ final class AssocArraySpec extends ObjectBehavior
 
         $filtered = $this->filterEmpty();
         $filtered->shouldNotBe($this);
-        $filtered->shouldBeLike(new AssocArray([
-            'a' => 'alf',
-            'c' => 'clifford',
-        ]));
+        $filtered->shouldBeLike(
+            new AssocArray([
+                'a' => 'alf',
+                'c' => 'clifford',
+            ])
+        );
     }
 
     function it_should_return_number_of_elements()
@@ -51,22 +57,26 @@ final class AssocArraySpec extends ObjectBehavior
     {
         $reversed = $this->reverse();
         $reversed->shouldNotBe($this);
-        $reversed->shouldBeLike(new AssocArray([
-            'c' => 'clifford',
-            'b' => 'berni',
-            'a' => 'alf',
-        ]));
+        $reversed->shouldBeLike(
+            new AssocArray([
+                'c' => 'clifford',
+                'b' => 'berni',
+                'a' => 'alf',
+            ])
+        );
     }
 
     function it_return_shuffled_array()
     {
         $shuffled = $this->shuffle();
         $shuffled->shouldNotBe($this);
-        $shuffled->shouldNotBeLike(new AssocArray([
-            'a' => 'alf',
-            'b' => 'berni',
-            'c' => 'clifford',
-        ]));
+        $shuffled->shouldNotBeLike(
+            new AssocArray([
+                'a' => 'alf',
+                'b' => 'berni',
+                'c' => 'clifford',
+            ])
+        );
     }
 
     function it_return_unique_values()
@@ -79,32 +89,38 @@ final class AssocArraySpec extends ObjectBehavior
     {
         $merged = $this->merge(new AssocArray(['d' => 'dummy']));
         $merged->shouldNotBe($this);
-        $merged->shouldBeLike(new AssocArray([
-            'a' => 'alf',
-            'b' => 'berni',
-            'c' => 'clifford',
-            'd' => 'dummy'
-        ]));
+        $merged->shouldBeLike(
+            new AssocArray([
+                'a' => 'alf',
+                'b' => 'berni',
+                'c' => 'clifford',
+                'd' => 'dummy'
+            ])
+        );
     }
 
     function it_can_delete_element_by_key()
     {
         $array = $this->without('a');
         $array->shouldNotBe($this);
-        $array->shouldBeLike(new AssocArray([
-            'b' => 'berni',
-            'c' => 'clifford',
-        ]));
+        $array->shouldBeLike(
+            new AssocArray([
+                'b' => 'berni',
+                'c' => 'clifford',
+            ])
+        );
     }
 
     function it_can_delete_element_by_value()
     {
         $array = $this->withoutElement('alf');
         $array->shouldNotBe($this);
-        $array->shouldBeLike(new AssocArray([
-            'b' => 'berni',
-            'c' => 'clifford',
-        ]));
+        $array->shouldBeLike(
+            new AssocArray([
+                'b' => 'berni',
+                'c' => 'clifford',
+            ])
+        );
     }
 
     function it_can_return_value_by_key()
@@ -146,5 +162,113 @@ final class AssocArraySpec extends ObjectBehavior
         $callableMock->__invoke('clifford', 'c')->shouldBeCalled();
 
         $this->each($callableMock);
+    }
+
+    function it_transforms_all_keys_to_strings()
+    {
+        $this->beConstructedWith(['zero', 'one', 'two']);
+
+        $this->keys()->shouldBeLike(Arrays::strings(['0', '1', '2']));
+    }
+
+    function it_can_be_created_from_array()
+    {
+        $this->beConstructedThrough('fromArray', [['a' => 'alf', 'b' => 'bernie']]);
+    }
+
+    function it_can_be_map_values_with_transformer_callback()
+    {
+        $mapped = $this->map(function (string $value, string $key): string {
+            return "{$key}: {$value}";
+        });
+
+        $mapped->shouldNotBe($this);
+        $mapped->shouldBeLike(
+            new AssocArray([
+                'a' => 'a: alf',
+                'b' => 'b: berni',
+                'c' => 'c: clifford',
+            ])
+        );
+    }
+
+    function it_can_map_keys_with_transformer_callback()
+    {
+        $mapped = $this->mapKeys(function (string $key, string $value): string {
+            return "{$key}: {$value}";
+        });
+
+        $mapped->shouldNotBe($this);
+        $mapped->shouldBeLike(
+            new AssocArray([
+                'a: alf' => 'alf',
+                'b: berni' => 'berni',
+                'c: clifford' => 'clifford',
+            ])
+        );
+    }
+
+    function it_can_filter_with_callback_filter()
+    {
+        $filtered = $this->filter(function (string $value): bool {
+            return $value === 'alf';
+        });
+
+        $filtered->shouldNotBe($this);
+        $filtered->shouldBeLike(new AssocArray(['a' => 'alf']));
+    }
+
+    function it_can_be_sorted_with_comparator()
+    {
+        $reversed = $this->sort(function(string $a, string $b): int {
+            return $b <=> $a;
+        });
+
+        $reversed->shouldNotBe($this);
+        $reversed->shouldBeLike(
+            new AssocArray([
+                'c' => 'clifford',
+                'b' => 'berni',
+                'a' => 'alf',
+            ])
+        );
+    }
+
+    function it_can_be_sorted_by_key_with_comparator()
+    {
+        $reversed = $this->sortKeys(function(string $a, string $b): int {
+            return $b <=> $a;
+        });
+
+        $reversed->shouldNotBe($this);
+        $reversed->shouldBeLike(
+            new AssocArray([
+                'c' => 'clifford',
+                'b' => 'berni',
+                'a' => 'alf',
+            ])
+        );
+    }
+
+    function it_can_reduce_items_with_callback()
+    {
+        $this->reduce(
+            function(string $reduced, string $value, string $key): string {
+                return $reduced . "{$key}: {$value} ";
+            },
+            ''
+        )->shouldReturn('a: alf b: berni c: clifford ');
+    }
+
+    function it_can_tell_if_is_not_empty()
+    {
+        $this->isEmpty()->shouldReturn(false);
+    }
+
+    function it_can_tell_if_is_empty()
+    {
+        $this->beConstructedWith([]);
+
+        $this->isEmpty()->shouldReturn(true);
     }
 }
