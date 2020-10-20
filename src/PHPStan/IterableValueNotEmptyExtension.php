5f749a939785453f83f2d92b91dcc289a4e81f22
Bronisław Białek <after89@gmail.com>
2020-10-20 09:56:47 +0200

    Configure repo as PHPStan extension (#32)

---
+++ src/PHPStan/IterableValueNotEmptyExtension.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace GW\Value\PHPStan;
+
+use GW\Value\IterableValue;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Type;
+
+final class IterableValueNotEmptyExtension implements DynamicMethodReturnTypeExtension
+{
+    use NotEmptyTypeRemover;
+
+    public function getClass(): string
+    {
+        return IterableValue::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'notEmpty';
+    }
+
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        return $this->removeNull($scope->getType($methodCall->var));
+    }
+}
